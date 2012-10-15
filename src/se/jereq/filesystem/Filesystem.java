1bcbb1c6ba846741d10cada48ff60f79adde998a
Sebastian Larsson <sjereq@gmail.com>
2012-10-15 21:04:54 +0200

    Implement Filesystem.ls using several useful helper functions.

--- src/se/jereq/filesystem/Filesystem.java
+++ src/se/jereq/filesystem/Filesystem.java
@@ -1,10 +1,16 @@
 package se.jereq.filesystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import se.jereq.filesystem.INode.Type;
+
 public class Filesystem
 {
 	private BlockDevice m_BlockDevice;
 
-	private String[] currentDirectory = new String[0];
+	private List<String> currentDirectory;
+	private int currentNode = -1;
 
 	public Filesystem(BlockDevice p_BlockDevice)
 	{
@@ -16,6 +22,9 @@ public class Filesystem
 		INode root = new INode("", INode.Type.Directory);
 		m_BlockDevice.writeBlock(0, root.getBlock());
 		dumpINode(root);
+		
+		currentDirectory = new ArrayList<String>();
+		currentNode = 0;
 		
 		return new String("Diskformat successful");
 	}
@@ -61,15 +70,111 @@ public class Filesystem
 		
 		dumpChildren(node);
 	}
+	
+	private INode findChildNode(INode current, String next)
+	{
+		if (current.getType() == Type.File)
+			return null;
+		
+		int i = 0;
+		int currChild = current.getChild(i++);
+		while (currChild != -1)
+		{
+			INode child = new INode(m_BlockDevice.readBlock(currChild));
+			
+			if (child.getName().equals(next))
+				return child;
+			
+			currChild = current.getChild(i++);
+		}
+		
+		return null;
+	}
+	
+	private String[] toAbsolute(String[] path)
+	{
+		if (path == null || path.length == 0)
+			return currentDirectory.toArray(new String[0]);
+		
+		List<String> absolutePath;
+		if ("".equals(path[0]))
+			absolutePath = new ArrayList<String>();
+		else
+			absolutePath = new ArrayList<String>(currentDirectory);
+		
+		for (String s : path)
+		{
+			if ("".equals(s) || ".".equals(s))
+				continue;
+			else if ("..".equals(s))
+				absolutePath.remove(absolutePath.size() - 1);
+			else
+				absolutePath.add(s);
+		}
+		
+		return absolutePath.toArray(new String[0]);
+	}
+	
+	private INode findINode(String[] path)
+	{
+		String[] absolutePath = toAbsolute(path);
+		
+		INode node = new INode(m_BlockDevice.readBlock(0));
+		for (String s : absolutePath)
+		{
+			node = findChildNode(node, s);
+			
+			if (node == null)
+				return null;
+		}
+		
+		return node;
+	}
 
 	public String ls(String[] p_asPath)
 	{
-		System.out.print("Listing directory ");
-		dumpArray(p_asPath);
-		System.out.print("");
-		return new String("");
+		if (currentNode < 0)
+			return "Invalid filesystem. Use format or read to prepare the filesystem before use.";
+		
+		INode dir = findINode(p_asPath);
+		
+		if (dir == null)
+		{
+			return "Directory does not exist";
+		}
+		
+		if (dir.getType() == Type.File)
+		{
+			return "Cannot list file";
+		}
+		
+		StringBuilder res = new StringBuilder("Listing directory /");
+		for (String p : toAbsolute(p_asPath))
+			res.append(p).append("/");
+		
+		res.append("\n\n");
+		
+		int i = 0;
+		int currChild = dir.getChild(i++);
+		while (currChild != -1)
+		{
+			INode child = new INode(m_BlockDevice.readBlock(currChild));
+			
+			String name = child.getName();
+			
+			res.append(name).append(new String(new char[20 - name.length()]).replace('\0', ' '));
+			res.append(child.getType()).append("\n");
+			
+			currChild = dir.getChild(i++);
+		}
+		
+		if (i == 1)
+		{
+			res.append("Empty directory");
+		}
+		
+		return res.toString();
 	}
-
 
 	public String create(String[] p_asPath,byte[] p_abContents)
 	{
@@ -155,6 +260,9 @@ public class Filesystem
 
 	public String pwd()
 	{
+		if (currentDirectory == null)
+			return "Unformatted filesystem";
+		
 		StringBuilder res = new StringBuilder("/");
 		for (String s : currentDirectory)
 		{
@@ -165,9 +273,9 @@ public class Filesystem
 
 	private void dumpArray(String[] p_asArray)
 	{
-		for(int nIndex=0;nIndex<p_asArray.length;nIndex++)
+		for(int nIndex = 0; nIndex < p_asArray.length; nIndex++)
 		{
-			System.out.print(p_asArray[nIndex]+"=>");
+			System.out.print(p_asArray[nIndex] + "=>");
 		}
 	}
 
